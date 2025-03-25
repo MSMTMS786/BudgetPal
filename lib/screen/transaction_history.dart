dbb44a4cd7c0753c700e57640125d1a85eecf31f
Misbah Ur Rehman <21-cs-24@students.uettaxila.edu.pk>
2025-03-25 22:19:42 +0500

    Some changes

--- lib/screen/transaction_history.dart
+++ lib/screen/transaction_history.dart
@@ -1,5 +1,4 @@
 import 'package:expense_tracker/model/model.dart';
-import 'package:expense_tracker/screen/add_expense_income.dart';
 import 'package:expense_tracker/screen/reciept_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
